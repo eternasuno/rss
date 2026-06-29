fef14b4b04d41a234e6b35dd5328727f95034fb8
eternasuno <22316214+eternasuno@users.noreply.github.com>
2026-06-29 21:27:40 +0900

    rebuild
    Signed-off-by: eternasuno <22316214+eternasuno@users.noreply.github.com>

--- apps/web/src/lib/db.ts
+++ apps/web/src/lib/db.ts
@@ -1,7 +1,10 @@
 import { drizzle } from 'drizzle-orm/tursodatabase/database';
-import { relations } from './relations';
+import { relations } from './db/relations';
 
 export const db = drizzle({
   connection: { path: process.env.DATABASE_URL || './.data/sqlite.db' },
   relations,
 });
+
+export { relations } from './db/relations';
+export * as schema from './db/schema';
